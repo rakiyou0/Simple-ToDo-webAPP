c4b4e08049442322c0e20df2ac0af7bdf1c3d0a7
Yasuhiro Kimura <Raki001@outlook.jp>
2023-05-02 15:59:54 +0900

    Add input Data

---
+++ app.js
@@ -0,0 +1,6 @@
+$("#addToDo").click(function () {
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("input").val("");
+});
